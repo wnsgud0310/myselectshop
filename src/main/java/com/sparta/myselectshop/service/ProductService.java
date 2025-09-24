c8cbf0bf3ef3ea7b8457cdcc1403cab3b4b1ee1a
wnsgud0310 <ffjjo0310@naver.com>
2025-09-25 02:01:52 +0900

    관심상품 조회 API 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
@@ -40,5 +43,16 @@ public class ProductService {
 
         return new ProductResponseDto(product);
 
+    }
+
+    public List<ProductResponseDto> getProducts() {
+        List<Product> productList = productRepository.findAll();
+        List<ProductResponseDto> responseDtoList = new ArrayList<>();
+
+        for (Product product : productList) {
+            responseDtoList.add(new ProductResponseDto(product));
+
+        }
+        return responseDtoList;
     }
 }
